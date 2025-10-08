483d539b441e27d9de09755bc5da10cb26087b84
mariag122 <felipedossantos2006@gmail.com>
2025-10-08 16:58:12 -0300

    cadastro e exclusão de cliente

---
+++ model/bean/Fornecedor.php
@@ -0,0 +1,45 @@
+<?php
+    class Fornecedor {
+        //Atributos
+        private $id;
+        private $cnpj;
+        private $razaoSocial;
+        private $email;
+        private $telefone;
+
+        //Métodos
+        public function getId(){
+            return $this ->id;
+        }
+        public function setId($id) {
+            $this -> id = $id;
+        }
+          public function getCnpj(){
+            return $this ->cnpj;
+        }
+        public function setCnpj($cnpj) {
+            $this -> cnpj =$cnpj;
+        }
+          public function getRazaoSocial(){
+            return $this ->razaoSocial;
+        }
+        public function setRazaoSocial($razaoSocial) {
+            $this -> razaoSocial = $razaoSocial;
+        }
+         public function getEmail(){
+            return $this ->email;
+        }
+        public function setEmail($email) {
+            $this -> email = $email;
+        }
+        public function getTelefone(){
+            return $this -> telefone;
+        }
+        public function setTelefone($telefone){
+            $this ->telefone = $telefone;
+        }
+        //
+        public function __toString(){
+            return $this->razaoSocial;
+        }
+    }
